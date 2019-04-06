42da8718e9e1f067303e35cff39dde8facbc6b7d
astonm <45306215+1astonm1@users.noreply.github.com>
2019-04-06 19:18:17 +0800

    2019.4.6.19.18

--- HDFS/HdfsTest/src/HdfsOperation/Hdfs.java
+++ HDFS/HdfsTest/src/HdfsOperation/Hdfs.java
@@ -1,13 +1,11 @@
 package HdfsOperation;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
 import org.apache.hadoop.io.IOUtils;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
@@ -15,6 +13,7 @@ import java.net.URLConnection;
 public class Hdfs {
 
     //读取
+    //
     public void readFile(String dir, String fileName) throws Exception {
         URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
         URL url = new URL("Hdfs://192.168.2.129:9000"+dir+fileName);
@@ -30,15 +29,60 @@ public class Hdfs {
     }
 
     //通过调用api读取文件
-    public void readFileByAPI() throws Exception {
+    public void readFileByAPI(String dir, String fileName) throws Exception {
         Configuration conf = new Configuration();
-        conf.set("fd.defaultFS", "Hdfs://192.168.2.129:9000/");
+        conf.set("fs.defaultFS", "hdfs://192.168.2.129:9000/");
         FileSystem fs = FileSystem.get(conf);
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        Path p = new Path("/user/hadoop/test.txt");
+        Path p = new Path(dir+fileName);
         FSDataInputStream fsDataInputStream = fs.open(p);
-        IOUtils.copyBytes(fsDataInputStream,byteArrayOutputStream,1024);
-        System.out.println(byteArrayOutputStream.toByteArray());
+        byte[] buf = new byte[1024];
+        int len = -1;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        while ((len = fsDataInputStream.read(buf)) != -1) {
+            byteArrayOutputStream.write(buf, 0, len);
+        }
+        fsDataInputStream.close();
+        byteArrayOutputStream.close();
+        System.out.println(new String(byteArrayOutputStream.toByteArray()));
+    }
+
+
+    public void readFileByAPI2(String dir, String fileName) throws Exception {
+        Configuration conf = new Configuration();
+        conf.set("fs.defaultFS", "hdfs://192.168.2.129:9000/");
+        FileSystem fs = FileSystem.get(conf) ;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        Path p = new Path(dir+fileName);
+        FSDataInputStream fis = fs.open(p);
+        IOUtils.copyBytes(fis, baos, 1024);
+        System.out.println(new String(baos.toByteArray()));
+    }
+
+    //在hdfs中创建文件夹
+    public void mkdir(String dir) throws Exception{
+        Configuration conf = new Configuration();
+        conf.set("fs.defaultFS", "hdfs://192.168.2.129:9000/");
+        FileSystem fs = FileSystem.get(conf) ;
+        fs.mkdirs(new Path(dir));
+    }
+
+    //上传文件
+    public void putFile(String dir, String fileName) throws Exception{
+        Configuration conf = new Configuration();
+        conf.set("fs.defaultFS", "hdfs://192.168.2.129:9000/");
+        FileSystem fs = FileSystem.get(conf) ;
+        FSDataOutputStream out = fs.create(new Path(dir+fileName));
+        out.write("helloworld".getBytes());
+        out.close();
+    }
+    //删除文件
+    public void removeFile() throws Exception{
+        Configuration conf = new Configuration();
+        conf.set("fs.defaultFS", "hdfs://192.168.2.129:9000/");
+        FileSystem fs = FileSystem.get(conf) ;
+        Path p = new Path("/user/hadoop/myhadoop");
+        fs.delete(p, true);
     }
 
 }
--- HDFS/HdfsTest/src/HdfsTest.java
+++ HDFS/HdfsTest/src/HdfsTest.java
@@ -2,7 +2,11 @@ import HdfsOperation.Hdfs;
 public class HdfsTest {
     public static void main(String[] args) throws Exception {
         Hdfs h1 = new Hdfs();
-        h1.readFile("/user/hadoop/", "test.txt");
-
+        h1.readFileByAPI();
+        h1.readFileByAPI2();
+        h1.mkdir();
+        h1.putFile();
+        h1.readFile("/user/hadoop/myhadoop/", "a.txt");
+        h1.removeFile();
     }
 }
