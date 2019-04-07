c98740730e85ab1e9ee29df657fb6057fc9e3e72
astonm <45306215+1astonm1@users.noreply.github.com>
2019-04-07 23:02:14 +0800

    2019.4.7.23.02

--- Hadooplearning/MapReduceTest/src/main/java/WCMapReduce.java
+++ Hadooplearning/MapReduceTest/src/main/java/WCMapReduce.java
@@ -4,19 +4,20 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
 
 public class WCMapReduce {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf);    //实例化作业
 
         //设置job的各种属性
         job.setJarByClass(WCMapReduce.class);   //设置jar 类
         job.setJobName("WordCount");            //设置作业名称
-        job.setInputFormatClass(FileInputFormat.class); //设置输入类型
+        job.setInputFormatClass(TextInputFormat.class); //设置输入类型
 
         FileInputFormat.addInputPath(job, new Path(args[0]));   //添加输入路径
         FileOutputFormat.setOutputPath(job, new Path(args[1])); //设置输出路径
@@ -30,5 +31,6 @@ public class WCMapReduce {
         job.setMapOutputKeyClass(Text.class);   //设置map输出key类型
         job.setMapOutputValueClass(IntWritable.class);  //设置map输出value类型
 
+        job.waitForCompletion(true);
     }
 }
