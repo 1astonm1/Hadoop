a6c35173b6e825f5f90196f096655d3df96461d9
astonm <45306215+1astonm1@users.noreply.github.com>
2019-04-23 19:32:23 +0800

    2019.4.23.19.32

---
+++ MapreduceLearning/MaxTemp/src/main/java/MaxTempMapper.java
@@ -0,0 +1,8 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class MaxTempMapper extends Mapper<LongWritable, Text, IntWritable, IntWritable> {
+    
+}
