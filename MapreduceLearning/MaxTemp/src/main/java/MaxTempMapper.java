8a7932410ecb11dec5c8deba63ce8e2e47564864
astonm <45306215+1astonm1@users.noreply.github.com>
2019-04-24 17:03:44 +0800

    2019.4.24.17.03

--- MapreduceLearning/MaxTemp/src/main/java/MaxTempMapper.java
+++ MapreduceLearning/MaxTemp/src/main/java/MaxTempMapper.java
@@ -3,6 +3,21 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class MaxTempMapper extends Mapper<LongWritable, Text, IntWritable, IntWritable> {
+import java.io.IOException;
 
+public class MaxTempMapper extends Mapper<IntWritable, IntWritable, IntWritable, IntWritable> {
+    @Override
+    protected void map(IntWritable key, IntWritable value, Context context) throws IOException, InterruptedException {
+        IntWritable keyOut = new IntWritable();
+        IntWritable valueOut = new IntWritable();
+
+        String arr[] = key.toString().split(" ");
+
+        for(String s:arr){
+            keyOut = arr[0];
+            valueOut = arr[1];
+        }
+
+
+    }
 }
